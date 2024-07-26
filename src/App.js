7daececccb45449ff67a484c17e983dd86c809df
keymo15 <modi.ke@northeastern.edu>
2024-07-26 12:00:00 -0400

    Optimized UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import './App.css';
-import Navbar from './components/Navbar.js';
-import Marketplace from './components/Marketplace';
-import Profile from './components/Profile';
-import SellNFT from './components/SellNFT';
-import NFTPage from './components/NFTpage';
-import ReactDOM from "react-dom/client";
+import './App.css'; // Importing the CSS file for styling
+import Navbar from './components/Navbar.js'; // Importing the Navbar component
+import Marketplace from './components/Marketplace'; // Importing the Marketplace component
+import Profile from './components/Profile'; // Importing the Profile component
+import SellNFT from './components/SellNFT'; // Importing the SellNFT component
+import NFTPage from './components/NFTpage'; // Importing the NFTPage component
+import ReactDOM from "react-dom/client"; // Importing ReactDOM for rendering the app
 import {
-  BrowserRouter,
-  Routes,
-  Route,
+  BrowserRouter, // Importing BrowserRouter for routing capabilities
+  Routes, // Importing Routes to define the different routes of the application
+  Route, // Importing Route to create individual routes
 } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="container">
+    <div className="container"> 
+        {/* Defining the routes for different components */}
         <Routes>
-          <Route path="/" element={<Marketplace />}/>
-          <Route path="/nftPage" element={<NFTPage />}/>        
-          <Route path="/profile" element={<Profile />}/>
-          <Route path="/sellNFT" element={<SellNFT />}/>             
+          <Route path="/" element={<Marketplace />}/> {/* Route for the Marketplace component */}
+          <Route path="/nftPage" element={<NFTPage />}/> {/* Route for the NFTPage component */}
+          <Route path="/profile" element={<Profile />}/> {/* Route for the Profile component */}
+          <Route path="/sellNFT" element={<SellNFT />}/> {/* Route for the SellNFT component */}
         </Routes>
     </div>
   );
 }
 
-export default App;
+export default App; // Exporting the App component as default
